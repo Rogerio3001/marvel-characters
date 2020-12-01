590d1594703670172c6f3422c59e1d4519bf4788
Rogerio3001 <Rogeriovinicios3001@gmail.com>
2020-12-01 20:45:42 -0300

    chore: setup project tools

---
+++ src/screens/signin/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Title } from './styles'
+
+function Signin() {
+  return (
+    <div className="signin">
+      <Title>Testando</Title>
+    </div>
+  )
+}
+
+export default Signin
