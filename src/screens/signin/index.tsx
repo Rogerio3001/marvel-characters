09d3c040275c4e6daf5f013de669bc607ebc6a4a
Rogerio3001 <Rogeriovinicios3001@gmail.com>
2020-12-01 22:27:21 -0300

    chore: create signin screen

--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Title } from './styles'
+import { H1, Form, Div, Input, Button } from './styles'
 
 function Signin() {
   return (
-    <div className="signin">
-      <Title>Testando</Title>
-    </div>
+    <Div>
+      <H1>Dados de acesso</H1>
+      <Form /* onSubmit={handleSubmit} */>
+        <Input type="text" name="publicKey" defaultValue="Public Key" />
+        <Input type="text" name="privateKey" defaultValue="Private Key" />
+        <Button type="submit" /* disabled={isSubmitting || errors} */>
+          Acessar
+        </Button>
+      </Form>
+    </Div>
   )
 }
 
--- src/screens/signin/styles.ts
+++ src/screens/signin/styles.ts
@@ -1,9 +1,54 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
+const H1 = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  color: palevioletred;
+  color: #005a9c;
+  margin-bottom: 2rem;
+  font-weight: bold;
+  font-size: 3.4rem;
+`
+const Img = styled.img`
+  flex-grow: 1;
 `
 
-export { Title }
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Div = styled.div`
+  margin-top: 10%;
+  width: 50%;
+  margin-left: 25%;
+`
+
+const Input = styled.input`
+  margin-bottom: 2rem;
+  width: 100%;
+  height: 5.6rem;
+  border-radius: 1rem;
+  position: relative;
+  transition: 0.3s all;
+  font-size: 16px;
+  font-weight: 400;
+  color: #999999;
+  text-align: center;
+`
+const Button = styled.button`
+  width: 100%;
+  height: 5.6rem;
+  border-radius: 1rem;
+  position: relative;
+  background-color: rgba(0, 90, 156, 0.5);
+  transition: 0.5s all;
+  font-size: 16px;
+  font-weight: 400;
+
+  :hover {
+    background-color: rgba(0, 90, 156, 0.7);
+  }
+`
+
+export { H1, Img, Form, Div, Input, Button }
